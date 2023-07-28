3e716624aae5889bdc3d4e88e639cac984475b44
simonmuzariri <simonsammuzariri@gmail.com>
2023-07-28 11:54:55 -0700

    all

--- environ.c
+++ environ.c
@@ -2,76 +2,67 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: Structure containing arguments
  * Return: Always 0
  */
 int _myenv(info_t *info)
 {
-	print_list_str(info->env);
-	return (0);
+print_list_str(info->env);
+return (0);
 }
 
 /**
  * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
+ * @info: Structure containing arguments
+ * @name: environ name
  *
- * Return: the value
+ * Return: value
  */
 char *_getenv(info_t *info, const char *name)
 {
-	list_t *node = info->env;
-	char *p;
-
-	while (node)
-	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
-	}
-	return (NULL);
+list_t *node = info->env;
+char *a;
+while (node)
+{
+a = starts_with(node->str, name);
+if (a && *a)
+return (a);
+node = node->next;
+}
+return (NULL);
 }
 
 /**
  * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * @info: Structure containing arguments
  *  Return: Always 0
  */
 int _mysetenv(info_t *info)
 {
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
+if (info->argc != 3)
+{
+_eputs("Incorrect number of arguements\n");
+return (1);
 }
-
+if (_setenv(info, info->argv[1], info->argv[2]))
+return (0);
+return (1);
+}
 /**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * _myunsetenv - Remove an environment variable * @info: Structure containing arguments
  *  Return: Always 0
  */
 int _myunsetenv(info_t *info)
 {
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
+int a;
+if (info->argc == 1)
+{
+_eputs("Too few arguements.\n");
+return (1);
+}
+for (a = 1; a <= info->argc; a++)
+_unsetenv(info, info->argv[a]);
+return (0);
 }
 
 /**
--- exits.c
+++ exits.c
@@ -9,25 +9,25 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+int a, b;
+char *s = dest;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			dest[j] = '\0';
-			j++;
-		}
-	}
-	return (s);
+a = 0;
+while (src[a] != '\0' && a < n - 1)
+{
+dest[a] = src[a];
+a++;
+}
+if (a < n)
+{
+b = a;
+while (b < n)
+{
+dest[b] = '\0';
+b++;
+}
+}
+return (s);
 }
 
 /**
@@ -39,22 +39,22 @@ char *_strncpy(char *dest, char *src, int n)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+int a, b;
+char *s = dest;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+a = 0;
+b = 0;
+while (dest[a] != '\0')
+a++;
+while (src[b] != '\0' && b < n)
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+if (b < n)
+dest[a] = '\0';
+return (s);
 }
 
 /**
@@ -65,10 +65,10 @@ char *_strncat(char *dest, char *src, int n)
  */
 char *_strchr(char *s, char c)
 {
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
+do {
+if (*s == c)
+return (s);
+} while (*s++ != '\0');
 
-	return (NULL);
+return (NULL);
 }
